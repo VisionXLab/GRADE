94b4342aa3a01a11edb9b07f2cb114d1d4fb23b2
123sio <149552610+123sio@users.noreply.github.com>
2026-04-22 13:31:44 +0800

    Update consis.py

--- consis.py
+++ consis.py
@@ -6,6 +6,7 @@ from openai import OpenAI
 from concurrent.futures import ProcessPoolExecutor
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
+from PIL import Image
 
 # Function to encode the image
 def encode_image(image_path, target_size=1024, fmt="JPEG"):
